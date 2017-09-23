d886e23a1807f11c826b095df418ebb61520fb87
todfresser <xbox@lange-os.de>
2017-09-23 22:59:01 +0200

    Item: HealingField added

--- Smash/src/todfresser/smash/items/HealingField.java
+++ Smash/src/todfresser/smash/items/HealingField.java
@@ -1,5 +1,33 @@
 package todfresser.smash.items;
 
-public class HealingField {
+import org.bukkit.Material;
+
+import todfresser.smash.items.main.SmashItem;
+
+public class HealingField extends SmashItem{
+
+	@Override
+	public String getDisplayName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Material getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getmaxItemUses() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSpawnChance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 }
