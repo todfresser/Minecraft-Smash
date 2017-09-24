788a0a783c37181d3e76f311eea02e60965711aa
Henryk <Henryk@Henryks-PC.fritz.box>
2017-09-24 19:03:24 +0200

    Airstrike fixed

--- Smash/src/todfresser/smash/items/Airstrike.java
+++ Smash/src/todfresser/smash/items/Airstrike.java
@@ -6,14 +6,10 @@ import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import todfresser.smash.extrafunctions.PlayerFunctions;
-import todfresser.smash.extrafunctions.VectorFunctions;
 import todfresser.smash.items.main.SmashItem;
-import todfresser.smash.main.ParticleEffectCommands;
 import todfresser.smash.map.Game;
 import todfresser.smash.map.SmashPlayerData;
 import todfresser.smash.particles.ParticleEffect;
@@ -62,38 +58,30 @@ public class Airstrike extends SmashItem{
 				ParticleEffect.CLOUD.display(0.5f, 1.3f, 0.5f, 0.3f, 20, particleSpawn, 40);
 				if(zeit == 10 || itemClicked != 1) {
 					
-					playerdata.registerItemRunnable(new BukkitRunnable() {
-						double y = 15;
-						Location airstrikeLocation = player.getLocation().add(0, y, 0);
-						
-						@Override
-						public void run() {
-							
-							double x = Math.random()*5;
-							double z = Math.random()*5;
-							
-							playerdata.registerItemRunnable(new BukkitRunnable() {
-								
-								@Override
-								public void run() {
-									
-									ParticleEffect.FIREWORKS_SPARK.display(0.0f, 0.0f, 0.0f, 0.0f, 1, airstrikeLocation.add(x,y -= 0.5, z), 40);
-									for (Entity e : airstrikeLocation.getWorld().getNearbyEntities(airstrikeLocation, 1.5, 1.5, 1.5)){
-										if (e.getType().equals(EntityType.PLAYER)){
-											if (!((Player)e).getUniqueId().equals(player.getUniqueId())){
-												PlayerFunctions.playOutDamage(game, player, 12, false);
-											}
+
+					for(int i = 0;i < 20; i++) {
+						playerdata.registerItemRunnable(new BukkitRunnable() {
+							Location airstrikeLocation = player.getLocation().add( Math.random()*10-5, Math.random()*5 + 12.5,  Math.random()*10-5);
+
+							@Override
+							public void run() {
+								ParticleEffect.FIREWORKS_SPARK.display(0.0f, 0.0f, 0.0f, 0.0f, 1, airstrikeLocation.add(0, -0.5, 0), 40);
+								for (Entity e : airstrikeLocation.getWorld().getNearbyEntities(airstrikeLocation, 1.5, 1.5, 1.5)){
+									if (e.getType().equals(EntityType.PLAYER)){
+										if (!((Player)e).getUniqueId().equals(player.getUniqueId())){
+											PlayerFunctions.playOutDamage(game, player, 12, false);
 										}
 									}
-									if(y == 0) {
-										playerdata.cancelItemRunnable(this);
-										return;
-									}
 								}
-							}, 0, 1);
-						}
-					}, 0, 2);
+								if(airstrikeLocation.getY() <= 0 || !airstrikeLocation.getBlock().getType().equals(Material.AIR)) {
+									playerdata.cancelItemRunnable(this);
+									return;
+								}
+							}
+						}, 0, 1);	
+					}	
 					playerdata.cancelItemRunnable(this);
+							
 				}
 				
 				zeit += 0.5;
